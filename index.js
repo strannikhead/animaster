dfde4e69b95308ea0a939453cccbb0a5d3fa89ed
strannikhead <144272555+strannikhead@users.noreply.github.com>
2025-03-11 16:54:57 +0500

    add infinite animation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function animaster(){
             element.style.transitionDuration =  `${duration}ms`;
             element.classList.remove('hide');
             element.classList.add('show');
-    },
+        },
 
         fadeOut(element, duration) {
             element.style.transitionDuration =  `${duration}ms`;
@@ -22,31 +22,30 @@ function animaster(){
 
 
         /**
-     * Функция, передвигающая элемент
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     * @param translation — объект с полями x и y, обозначающими смещение блока
-     */
+         * Функция, передвигающая элемент
+         * @param element — HTMLElement, который надо анимировать
+         * @param duration — Продолжительность анимации в миллисекундах
+         * @param translation — объект с полями x и y, обозначающими смещение блока
+         */
         move(element, duration, translation) {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(translation, null);
-    },
+        },
 
-    /**
-     * Функция, увеличивающая/уменьшающая элемент
-     * @param element — HTMLElement, который надо анимировать
-     * @param duration — Продолжительность анимации в миллисекундах
-     * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
-     */
+        /**
+         * Функция, увеличивающая/уменьшающая элемент
+         * @param element — HTMLElement, который надо анимировать
+         * @param duration — Продолжительность анимации в миллисекундах
+         * @param ratio — во сколько раз увеличить/уменьшить. Чтобы уменьшить, нужно передать значение меньше 1
+         */
         scale(element, duration, ratio) {
             element.style.transitionDuration =  `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
         },
 
-        moveAndHide(element, duration) {
-            this.move(element, duration * 0.6);
-            setTimeout(() => this.fadeOut(element, duration * 0.4), duration * 0.6);
-
+        moveAndHide(element, duration, translation) {
+            this.move(element, duration / 3, translation);
+            setTimeout(() => this.fadeOut(element, duration / 3), duration / 3);
         },
 
         showAndHide(element, duration) {
@@ -55,10 +54,22 @@ function animaster(){
         },
 
         heartBeating(element, duration) {
-            this.scale(element, duration / 2, 1.4);
-            setTimeout(() => this.scale(element, duration / 2, 1), 500)
+            // this.scale(element, duration / 2, 1.4);
+            // setTimeout(() => this.scale(element, duration / 2, 1), 500)
+            const interval = setInterval(() => {
+                this.scale(element, 500, 1.4);
+                setTimeout(() => {
+                    this.scale(element, 500, 1);
+                }, 500);
+            }, 1000);
+            return {
+                stop: function () {
+                    clearInterval(interval);
+                }
+            };
+
         }
-}
+    }
 }
 function addListeners() {
     document.getElementById('fadeInPlay')
@@ -88,7 +99,7 @@ function addListeners() {
     document.getElementById('moveAndHide')
         .addEventListener('click', function () {
             const block = document.getElementById('moveAndHideBlock');
-            animaster().moveAndHide(block, 5000);
+            animaster().moveAndHide(block, 5000, {x: 100, y: 10});
         });
 
     document.getElementById('showAndHide')
